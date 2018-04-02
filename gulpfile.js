248503f6a9ef967cd5d3d35bb54d8d3c9a6be391
Toon Van de Putte <toon@automaton.be>
2018-04-02 23:27:03 +0200

    cleanup gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,6 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var cleanCSS = require('gulp-clean-css');
-var del = require('del');
-var rename = require("gulp-rename");
-var watch = require("gulp-watch");
-var gulpcopy = require("gulp-copy");
 
 gulp.task('get_site_less', function() {
     gulp.src(['../fri3d2018-announce/less/*.less'])
@@ -21,14 +17,14 @@ gulp.task('cssmin', function() {
 gulp.task('cssdev', function() {
     gulp.src(['less/*.less'])
         .pipe(less())
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('css'));
 });
 
 gulp.task('dev', ['get_site_less','cssdev']);
 
-gulp.task('default', ['get_site_less','cssmin']);
+gulp.task('default', ['dev','stream']);
 
 gulp.task('stream', function () {
     // Endless stream mode
-    gulp.watch(['less/*.less','less/includes/*.less'], ['get_site_less','cssdev']);
+    gulp.watch(['less/*.less','less/includes/*.less'], ['dev']);
 });
