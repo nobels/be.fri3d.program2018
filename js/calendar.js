82599b63a528f30452f35370be4cba4038a28944
Michael Smith <m@michaelsmith.be>
2018-06-22 15:16:34 +0200

    Switched Fullcalendar Scheduler plugin to GPL license. Removed commented out code.

--- js/calendar.js
+++ js/calendar.js
@@ -3,6 +3,7 @@ layout: 'jsfile'
 ---
 $(document).ready(function() {
     $('#calendar').fullCalendar({
+        schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
         customButtons: {
             day1: {
                 text: 'Zaterdag',
@@ -13,7 +14,7 @@ $(document).ready(function() {
                     $('#calendar').fullCalendar('getView');
                     $("#calendar").fullCalendar(
                         'changeView', $('#calendar').fullCalendar('getView').name, '2018-08-18'
-                    );
+                        );
                 }
             },
             day2: {
@@ -25,7 +26,7 @@ $(document).ready(function() {
                     $('#calendar').fullCalendar('getView');
                     $("#calendar").fullCalendar(
                         'changeView', $('#calendar').fullCalendar('getView').name, '2018-08-19'
-                    );
+                        );
                 }
             },
             day3: {
@@ -37,7 +38,7 @@ $(document).ready(function() {
                     $('#calendar').fullCalendar('getView');
                     $("#calendar").fullCalendar(
                         'changeView', $('#calendar').fullCalendar('getView').name, '2018-08-20'
-                    );
+                        );
                 }
             }
         },
@@ -53,101 +54,26 @@ $(document).ready(function() {
         maxTime: '23:00:00',
         locale: 'nl-be',
         height: 'auto',
-        groupByResource: true,
+        groupByResource: false,
         buttonIcons: true, // show the prev/next text
         weekNumbers: false,
         navLinks: true, // can click day/week names to navigate views
         editable: false,
         eventLimit: true, // allow "more" link when too many events
-  events: '{{site.baseurl}}/alltracks.json',
-  resources: [
-    { id: 'main-stage', title: 'Main Stage' },
-    { id: 'content', title: 'Content' },
-    { id: 'terrein', title: 'Terrein' },
-    { id: 'berkenhof', title: 'Berkenhof' },
-    { id: 'hardware-hacking-tent', title: 'Hardware Hacking Tent' },
-    { id: 'knutselbaar', title: 'KnutselBaar' },
-    { id: 'hoofdgebouw-refter', title: 'Hoofdgebouw Refter' },
-    { id: 'hoofdgebouw-room-2', title: 'Hoofdgebouw Room 2' },
-    { id: 'hoofdgebouw-room-3', title: 'Hoofdgebouw Room 3' },
-    { id: 'hoofdgebouw-room-4', title: 'Hoofdgebouw Room 4' },
-    { id: 'shelter', title: 'Shelter' },
-  ],
-
-        // eventSources: [
-
-        //     // your event source
-        //     {
-        //         id: 'a',
-        //         url: '{{site.baseurl}}/track1.json', // use the `url` property
-        //         className: 'track1',
-        //         //color: 'yellow',    // an option!
-        //         textColor: 'black' // an option!
-        //     },
-        //     {
-        //         url: '{{site.baseurl}}/track2.json', // use the `url` property
-        //         className: 'track2',
-        //         //color: 'green',    // an option!
-        //         textColor: 'black' // an option!
-        //     },
-        //     {
-        //         url: '{{site.baseurl}}/track3.json', // use the `url` property
-        //         className: 'track3',
-        //         //color: 'green',    // an option!
-        //         textColor: 'black' // an option!
-        //     },
-        //     {
-        //         url: '{{site.baseurl}}/track4.json', // use the `url` property
-        //         className: 'track4',
-        //         //color: 'green',    // an option!
-        //         textColor: 'black' // an option!
-        //     },
-        //     {
-        //         url: '{{site.baseurl}}/track5.json', // use the `url` property
-        //         className: 'track5',
-        //         //color: 'green',    // an option!
-        //         textColor: 'black' // an option!
-        //     },
-        //     {
-        //         url: '{{site.baseurl}}/track6.json', // use the `url` property
-        //         className: 'track6',
-        //         //color: 'green',    // an option!
-        //         textColor: 'black' // an option!
-        //     },
-        //     {
-        //         url: '{{site.baseurl}}/track7.json', // use the `url` property
-        //         className: 'track7',
-        //         //color: 'green',    // an option!
-        //         textColor: 'black' // an option!
-        //     },
-        //     {
-        //         url: '{{site.baseurl}}/track8.json', // use the `url` property
-        //         className: 'track8',
-        //         //color: 'green',    // an option!
-        //         textColor: 'black' // an option!
-        //     },
-        //     {
-        //         url: '{{site.baseurl}}/track9.json', // use the `url` property
-        //         className: 'track9',
-        //         //color: 'green',    // an option!
-        //         textColor: 'black' // an option!
-        //     },
-        //     {
-        //         url: '{{site.baseurl}}/track10.json', // use the `url` property
-        //         className: 'track10',
-        //         //color: 'green',    // an option!
-        //         textColor: 'black' // an option!
-        //     },
-        //     {
-        //         url: '{{site.baseurl}}/track11.json', // use the `url` property
-        //         className: 'track11',
-        //         //color: 'green',    // an option!
-        //         textColor: 'black' // an option!
-        //     },
-
-        //     // any other sources...
-
-        // ],
+        events: '{{site.baseurl}}/alltracks.json',
+        resources: [
+            { id: 'main-stage', title: 'Main Stage' },
+            { id: 'content', title: 'Content' },
+            { id: 'terrein', title: 'Terrein' },
+            { id: 'berkenhof', title: 'Berkenhof' },
+            { id: 'hardware-hacking-tent', title: 'Hardware Hacking Tent' },
+            { id: 'knutselbaar', title: 'KnutselBaar' },
+            { id: 'hoofdgebouw-refter', title: 'Hoofdgebouw Refter' },
+            { id: 'hoofdgebouw-room-2', title: 'Hoofdgebouw Room 2' },
+            { id: 'hoofdgebouw-room-3', title: 'Hoofdgebouw Room 3' },
+            { id: 'hoofdgebouw-room-4', title: 'Hoofdgebouw Room 4' },
+            { id: 'shelter', title: 'Shelter' },
+        ],
 
         eventAfterAllRender: function() {
             if ( !$(".fc-day2-button").hasClass("fc-state-active") && !$(".fc-day3-button").hasClass("fc-state-active") ) {
@@ -178,16 +104,6 @@ $(document).ready(function() {
     });
 
     $(".popover-wrapper").hide();
-
-/*
-    $(".toggletrack").hover(function(e) {
-        if ( "visibility_off" != $(this).children(".material-icons").text() ) {
-            $(this).children(".material-icons").text("visibility_off");
-        } else {
-            $(this).children(".material-icons").text("visibility");
-        }
-    });
-*/
     $(".toggletrack").click(function(e) {
         e.preventDefault();
         var trackname = $(this).data('track');
@@ -206,5 +122,4 @@ $(document).ready(function() {
             $(this).children(".material-icons").text("visibility");
         }
     });
-
 });
